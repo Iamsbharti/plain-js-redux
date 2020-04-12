7fb0ec959f483a75a26148928d51769ad322ac89
Saurabh Bharti <30685271+Iamsbharti@users.noreply.github.com>
2020-04-12 17:24:50 +0530

    implemented redux to plain js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
 import "./styles.css";
+const redux = require("redux");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const initialState = {
+  count: 0,
+  favoriteNumber: []
+};
+
+function changeCount(changeType = "INC-DEC", amount = 3) {
+  return { type: changeType, payload: amount };
+}
+function addFavorite(favoriteNumber) {
+  return { type: "ADD-FAVORITE", payload: favoriteNumber };
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "INC-DEC":
+      return {
+        ...state,
+        count: state.count + action.payload
+      };
+    case "ADD-FAVORITE":
+      return {
+        ...state,
+        favoriteNumber: [...state.favoriteNumber, action.payload]
+      };
+    case "DOUBLE":
+      return {
+        ...state,
+        count: state.count * 2
+      };
+    case "HALVE":
+      return {
+        ...state,
+        count: Math.random(state.count / 2)
+      };
+    default:
+      return state;
+  }
+}
+
+//configure store
+const store = redux.createStore(reducer);
+store.subscribe(() => {
+  console.log(store.getState());
+});
+//dispatch actions
+store.dispatch(changeCount("INC-DEC", 3));
+store.dispatch(changeCount("DOUBLE"));
+store.dispatch(addFavorite(4));
